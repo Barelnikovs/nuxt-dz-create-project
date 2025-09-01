222431d651cdceaf5248a9ecbc0defa4d6aabf94
Daniil Barelnikov <daniilbarelnikov@gmail.com>
2025-09-01 19:36:49 +0300

    Сделал загрузку комментов
    Сделал загрузку комментов на главной и загрузку одного коммента на /page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import type { GetCommentData } from '~/interfaces/comment.interfaces';
+
+const runtimeConfig = useRuntimeConfig()
+const APIURL = runtimeConfig.public.APIURL
+
+const { data } = await useFetch<GetCommentData>(`${APIURL}/posts`)
+</script>
+
 <template>
-    <div>Home</div>
+    <div class="wrapper">
+        <Comment v-for="comment in data?.posts" :key="comment.id" v-bind="comment" />
+    </div>
 </template>
+
+<style scoped>
+.wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+}
+</style>
