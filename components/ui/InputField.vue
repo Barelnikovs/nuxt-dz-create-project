8d4d8db1ec58453c80c83ade4cf287fc79985bb9
Daniil Barelnikov <daniilbarelnikov@gmail.com>
2025-09-16 10:49:28 +0300

    Сделал компонент инпут

---
+++ components/ui/InputField.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{ placeholder: string }>()
+</script>
+
+<template>
+    <div class="input__wrapper">
+        <input :placeholder="placeholder">
+    </div>
+</template>
+
+<style scoped>
+.input__wrapper {
+    border: 1px solid var(--color-black);
+    border-radius: 23px;
+    padding: 12px;
+}
+
+.input__wrapper input {
+    border: none;
+    font-family: var(--font-main);
+    font-size: 16px;
+    width: 100%;
+}
+
+.input__wrapper input:focus {
+    outline: none;
+    border: none;
+}
+</style>
