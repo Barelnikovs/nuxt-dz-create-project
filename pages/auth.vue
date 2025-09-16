cbfee7f92165dec5c8620a7e3653f85329203c4f
Daniil Barelnikov <daniilbarelnikov@gmail.com>
2025-09-16 10:49:49 +0300

    Сверстал страницу auth.vue

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,3 +1,37 @@
+<script setup lang="ts">
+import BaseButton from '~/components/ui/BaseButton.vue';
+import InputField from '~/components/ui/InputField.vue';
+
+</script>
+
 <template>
-    <div>Auth</div>
+    <div class="auth__wrapper">
+        <form>
+            <h2>Вход на платформу</h2>
+            <InputField placeholder="Email" />
+            <InputField placeholder="Пароль" />
+            <BaseButton>Войти в аккаунт</BaseButton>
+        </form>
+    </div>
 </template>
+
+<style scoped>
+.auth__wrapper {
+    height: calc(100svh - 20px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.auth__wrapper form {
+    width: 300px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 10px;
+}
+
+.auth__wrapper h2 {
+    align-self: center;
+}
+</style>
